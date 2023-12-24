3f10e416f6e9cdab2dc99c3833f98ef987075f48
IDMDebi <154725677+IDMDebi@users.noreply.github.com>
2023-12-24 07:41:44 +0000

    Variables

--- test.js
+++ test.js
@@ -1,1 +1,8 @@
 console.log("Debiprasad")
+
+const account_id = 234533
+let account_Name = "Debiprasad"
+var accountCity = "Bhubaneswar"
+accountEmail = "work.debiprasad@google.com"
+
+console.table([account_id,account_Name,accountCity,accountEmail])
